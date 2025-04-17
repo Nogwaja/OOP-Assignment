aab52462a8f027e76622ff10136c0c32c65f0762
nogwaja <ave.tyeba@gmail.com>
2025-04-17 21:30:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Creating an object from the Pet class
+my_dog = Pet("Buddy")
+
+# Testing the object
+my_dog.get_status()
+my_dog.eat()
+my_dog.sleep()
+my_dog.play()
+my_dog.train("roll over")
+my_dog.train("sit")
+my_dog.show_tricks()
